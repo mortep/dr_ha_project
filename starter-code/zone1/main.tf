4c2d70a39defb11a3a4268609061924c444735e4
Jorge Morte <jmorte@zzdatalabs.com>
2022-01-29 21:10:52 +0200

    AZs fix

--- starter-code/zone1/main.tf
+++ starter-code/zone1/main.tf
@@ -30,7 +30,7 @@ module "vpc_west" {
 
   account_owner = local.name
   name          = "${local.name}-project"
-  azs           = ["us-west-1b", "us-west-1c"]
+  azs           = ["us-west-1a", "us-west-1b"]
   private_subnet_tags = {
     "kubernetes.io/role/internal-elb" = 1
   }
